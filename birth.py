3f9da487ebc915ce342365bee4a4219cb3d72170
SandeepaTN <90259803+SandeepaTN@users.noreply.github.com>
2022-11-29 15:48:23 +0530

    Update birth.py

--- birth.py
+++ birth.py
@@ -2,11 +2,12 @@ a = {}
 while True:
     x = int(input('1-check and add 2-exit'))
     if(x == 1 ):
-        name = input('enter friend name')
+        name = input('enter friend name ')
         if name in a:
                 print('birth day-'+str(a[name]))
         else :
-                 day=  input("enter friend's birthady")
-                 a.update(name ='day')
+                 day=  input("enter friend's birthady ")
+                 a.update({name :day})
     else:
          break
+         
